2097c5e211f26cd91a139a2906ee90a19e3554f2
Hubert <h.weydmann@plusautomatyka.pl>
2022-06-27 11:11:04 +0200

    added telegram builder sketch

---
+++ Models/TelegramBuilder.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+
+
+namespace CacheService.Models
+{
+    public class TelegramBuilder
+    {
+        private class Field
+        {
+            protected byte[] ByteArray { get; set; }
+            public uint dst_index { get; set; } = uint.MinValue;
+            public uint length { get; set; } = uint.MinValue;
+
+            public Field(string value, uint from_index, uint field_length)
+            {
+                ByteArray = Encoding.ASCII.GetBytes(value.PadRight((int)field_length, ' '));
+                dst_index = from_index;
+                length = field_length;
+            }
+
+            public byte[] Apply(byte[] arr)
+            {
+                if (arr == null) return arr;
+                if (dst_index > arr.Length) return arr;
+                if (dst_index + length > arr.Length) length = (uint) arr.Length - dst_index;
+                Array.Copy(ByteArray, 0, arr, dst_index, length);
+                return arr;
+            }
+
+        }
+
+        protected byte[] ByteArray { get; set; } = new byte[54];
+        private List<Field> fields = new List<Field>();
+
+        public TelegramBuilder()
+        {
+
+        }
+
+        public TelegramBuilder AddField(string value, uint from_index, uint field_length)
+        {
+            fields.Add(new Field(value, from_index, field_length));
+            return this;
+        }
+
+        protected virtual void AddPrefixSuffix() { }
+
+        public byte[] Build()
+        {
+            foreach (Field field in fields)
+            {
+                field.Apply(ByteArray);
+            }
+            AddPrefixSuffix();
+            return ByteArray;
+        }
+
+
+    }
+    public class Telegram54 : TelegramBuilder
+    {
+        private byte[] template;
+
+        public Telegram54()
+        {
+            template = new byte[54];
+            Array.Fill(template, (byte)' ');
+            ByteArray = template;
+        }
+
+        override protected void AddPrefixSuffix()
+        {
+            ByteArray[0] = (byte)'<';
+            ByteArray[53] = (byte)'>';
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,5 +1,32 @@
 using CacheService.Data;
+using CacheService.Models;
 using StackExchange.Redis;
+using System.Text;
+
+
+var t = new Telegram54()
+    .AddField("node", 1, 4)
+    .AddField("1234567890", 50, 10)
+    .Build();
+
+Console.WriteLine(Encoding.ASCII.GetString(t));
+
+//<1234CMD 3450000000FK132456                          >
+//000000000011111111112222222222333333333344444444445555
+//012345678901234567890123456789012345678901234567890123
+
+
+
+t = new Telegram54()
+    .AddField("1234", 1, 4)
+    .AddField("CMD", 5, 4)
+    .AddField("345", 9, 3)
+    .AddField("1234",12, 4).AddField("000F", 16, 4)
+    .AddField("K132456123456".PadRight(32, ' '), 20, 32)
+    .Build();
+
+Console.WriteLine(Encoding.ASCII.GetString(t));
+
 
 var builder = WebApplication.CreateBuilder(args);
 
